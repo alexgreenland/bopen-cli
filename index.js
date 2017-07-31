d55e81c7b7841ea98376ac00e9a619e48c222e58
Alex Greenland <alex+git@alexgreenland.com>
2017-07-31 22:43:22 +0100

    WIP

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ const bopen = require('bopen')
 const cli = require('commander')
 
 let list = (val) => {
-  return val.split(' ')
+  return val.split(' ').replace('-', 'hey')
 }
 
 cli
   .version(pkg.version)
-  .option('-i, --incognito', 'Open the URL in incognito/private mode', false)
-  .option('-b, --browser [name]', 'The browser to launch, such as "chrome", "ie", etc.')
-  .option('-a, --app [name]', 'The app to launch (platform-dependent name)')
-  .option('-t, --targs "[args...]"', 'The arguments to pass to the target application', list, [])
-  .option('-o, --oargs "[args...]"', 'The arguments to pass to the opener', list, [])
+  .option('-i, --incognito', 'open the URL in incognito/private mode', false)
+  .option('-b, --browser [name]', 'browser to launch, such as "chrome", "ie", etc.')
+  .option('-a, --app [name]', 'app to launch (platform-dependent name)')
+  .option('-t, --targs ["args..."]', 'arguments to the target application', list, [])
+  .option('-o, --oargs ["args..."]', 'arguments to the opener', list, [])
   .usage(`<url|path> [options]
   
     A better native open utility.
@@ -24,7 +24,8 @@ cli
     $ bopen http://example.com chrome
     $ bopen http://example.com firefox
     $ bopen http://example.com chrome --incognito
-    $ bopen http://example.com ie --incognito`)
+    $ bopen http://example.com ie --incognito
+    $ bopen http://example.com --targs "-n"`)
   .parse(process.argv)
   
 let location = cli.args[0]
